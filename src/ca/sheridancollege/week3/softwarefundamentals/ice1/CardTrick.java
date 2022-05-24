42201653b56fcf40048724951e1da86660b37aed
John <John@host.docker.internal>
2022-05-24 15:04:42 -0400

    Merge origin/master
    Final Product Finished! Again! - John Paua
    
    Conflicts:
    	src/gitproject/GitProject.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -48,6 +48,8 @@ public class CardTrick {
                     break;
             case 3: userCardSuit = "Clubs";
                     break;
+            default: userCardSuit = "Invalid Input";
+                    break;
         }*/
         //hardcoding luckycard
         Card luckyCard = new Card();
